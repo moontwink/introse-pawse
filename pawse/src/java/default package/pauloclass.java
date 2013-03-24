fced6f6b39f4ddff495a98862887888bb2639d3d
Jack <Jack@Jack-PC>
2013-03-25 03:52:56 +0800

    [no commit message]

---
+++ pawse/src/java/default package/pauloclass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gilpaulo
+ */
+public class pauloclass {
+    public int one(){
+        return 1;
+    }
+}
