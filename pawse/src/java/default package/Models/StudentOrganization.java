394b8fef671cdc7f3fe87b54ac6eeda8948cd8c6
gilpaulo <gilpaulo@spider>
2013-03-25 13:30:04 +0800

    [no commit message]

---
+++ pawse/src/java/default package/Models/StudentOrganization.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author gilpaulo
+ */
+public class StudentOrganization {
+    String name;
+    
+}
