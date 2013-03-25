f10f02d5899408ad3927f5467fbedfcfcd536d5e
MigzMeizter <MigzMeizter@MigzMeizter-PC>
2013-03-25 14:14:03 +0800

    [no commit message]

---
+++ pawse/src/java/default package/Models/Coordinator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author MigzMeizter
+ */
+public class Coordinator {
+    private String name;
+}
