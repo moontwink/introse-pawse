28bc28e1991192f7a8e9caa3a3b7df046effda4a
gilpaulo <gilpaulo@spider>
2013-03-28 23:24:47 +0800

    [no commit message]

---
+++ pawse/src/java/default package/Models/Coordinator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author MigzMeizter
+ */
+public class Coordinator {
+    private int id;
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
