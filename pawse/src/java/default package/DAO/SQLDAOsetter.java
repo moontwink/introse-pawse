a517300b81b774a27624fafe0d9b0556f1330fcd
MigzMeizter <MigzMeizter@MigzMeizter-PC>
2013-03-28 16:15:41 +0800

    [no commit message]

---
+++ pawse/src/java/default package/DAO/SQLDAOsetter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author MigzMeizter
+ */
+public class SQLDAOsetter extends SQLDAO {
+    
+   
+  /* public UserDAO createUserDAO() {
+        return new UserDAOsetter();
+    } */   
+
+}
