d55314564215ea5ebbead8e82c4b934a4f88d6bb
Nancy <Nancy@Nanchii-HP>
2013-03-28 15:47:33 +0800

    [no commit message]

---
+++ pawse/src/java/default package/Models/Accomplisher.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Nancy
+ */
+public class Accomplisher {
+    
+    private int idNumber;
+    private String name;
+    private String designation;
+    private String cellNo;
+    private String email;
+
+    /**
+     * @return the idNumber
+     */
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    /**
+     * @param idNumber the idNumber to set
+     */
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the designation
+     */
+    public String getDesignation() {
+        return designation;
+    }
+
+    /**
+     * @param designation the designation to set
+     */
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    /**
+     * @return the cellNo
+     */
+    public String getCellNo() {
+        return cellNo;
+    }
+
+    /**
+     * @param cellNo the cellNo to set
+     */
+    public void setCellNo(String cellNo) {
+        this.cellNo = cellNo;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
