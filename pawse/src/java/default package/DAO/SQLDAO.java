dd05f51db97c29a8e436ff5746fb622bec47549d
gilpaulo <gilpaulo@spider>
2013-03-28 18:40:30 +0800

    [no commit message]

--- pawse/src/java/default package/DAO/SQLDAO.java
+++ pawse/src/java/default package/DAO/SQLDAO.java
@@ -12,7 +12,7 @@ public abstract class SQLDAO {
     
       public static final int MYSQL = 1;
     public static final int ORACLE = 2;
-    
+     
     public static SQLDAO getInstance(int dataSource){
         switch (dataSource){
             case MYSQL: return new SQLDAOsetter();
