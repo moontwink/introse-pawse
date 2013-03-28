dd1054308f9ecd193988902b43a89f77b87448e3
gilpaulo <gilpaulo@spider>
2013-03-28 23:47:14 +0800

    [no commit message]

--- pawse/src/java/default package/DB/DBConnection.java
+++ pawse/src/java/default package/DB/DBConnection.java
@@ -28,7 +28,7 @@ public abstract class DBConnection {
                 pWord = "chykuri";
                 SQLdriver  = "com.mysql.jdbc.Driver";
                 SQLurl  = "jdbc:mysql://localhost:3306/outracker";
-               //test sssssssss
+               //test sssss
             break;
         }
         return new DBConnectionSetter();
